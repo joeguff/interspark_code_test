01050ffeee3b4eb5c1d7dee56dfd6aa3409ee3a9
Joseph Gustavson <joeguff@gmail.com>
2023-08-31 15:45:06 -0500

    Fix duplicate declerations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { JobListComponent } from './components/job-list/job-list.component';
 import { NewJobComponent } from './components/new-job/new-job.component';
 import { JobDetailsComponent } from './components/job-details/job-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JobListComponent,
     NewJobComponent,
     JobDetailsComponent
   ],
